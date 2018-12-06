7d50835baa34bc23bde69933074d6cf9ebf828a0
andrey <ukraineboard1@gmail.com>
2018-12-06 22:47:16 +0200

    Added user role

--- app/Entity/User.php
+++ app/Entity/User.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Str;
  * @property string $email
  * @property string $password
  * @property string $verify_token
+ * @property string $role
  * @property string $status
  */
 class User extends Authenticatable
@@ -19,11 +20,11 @@ class User extends Authenticatable
     use Notifiable;
     public const STATUS_WAIT = 'wait';
     public const STATUS_ACTIVE = 'active';
-
+    public const ROLE_USER = 'user';
+    public const ROLE_ADMIN = 'admin';
     protected $fillable = [
-        'name', 'email', 'password', 'status',
+        'name', 'email', 'password', 'verify_token', 'status', 'role',
     ];
-
     protected $hidden = [
         'password', 'remember_token',
     ];
@@ -35,6 +36,7 @@ class User extends Authenticatable
             'email' => $email,
             'password' => bcrypt($password),
             'verify_token' => Str::uuid(),
+            'role' => self::ROLE_USER,
             'status' => self::STATUS_WAIT,
         ]);
     }
@@ -45,6 +47,7 @@ class User extends Authenticatable
             'name' => $name,
             'email' => $email,
             'password' => bcrypt(Str::random()),
+            'role' => self::ROLE_USER,
             'status' => self::STATUS_ACTIVE,
         ]);
     }
@@ -68,5 +71,21 @@ class User extends Authenticatable
             'status' => self::STATUS_ACTIVE,
             'verify_token' => null,
         ]);
+    }
+
+    public function changeRole($role): void
+    {
+        if (!\in_array($role, [self::ROLE_USER, self::ROLE_ADMIN], true)) {
+            throw new \InvalidArgumentException('Undefined role "' . $role . '"');
+        }
+        if ($this->role === $role) {
+            throw new \DomainException('Role is already assigned.');
+        }
+        $this->update(['role' => $role]);
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === self::ROLE_ADMIN;
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Entity\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,12 +15,16 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
+    $active = $faker->boolean;
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
+        'verify_token' => $active ? null : Str::uuid(),
+        'role' => $active ? $faker->randomElement([User::ROLE_USER, User::ROLE_ADMIN]) : User::ROLE_USER,
+        'status' => $active ? User::STATUS_ACTIVE : User::STATUS_WAIT
     ];
 });
